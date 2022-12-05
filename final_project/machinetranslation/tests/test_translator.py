6d4cf99bd59e589573a3aea02b01a20b14ad62cd
starfile <114294607+starfile@users.noreply.github.com>
2022-12-05 23:25:57 +0100

    Fixed method names

--- final_project/machinetranslation/tests/test_translator.py
+++ final_project/machinetranslation/tests/test_translator.py
@@ -1,16 +1,16 @@
 import unittest
 
-from translator import frenchToEnglish
-from translator import englishToFrench
+from translator import french_to_english
+from translator import english_to_french
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
-        self.assertNotEqual(frenchToEnglish("Bonjour"), "Hello")
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(french_to_english("Bonjour"), "Hello")
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(englishToFrench("Hello"), "Bonjour")
-        self.assertNotEqual(englishToFrench("Hello"), "Bonjour")
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertNotEqual(english_to_french("Hello"), "Bonjour")
 
 unittest.main()
